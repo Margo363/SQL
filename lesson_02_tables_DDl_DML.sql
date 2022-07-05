245ddcf5d8198ce943fd94e2d8f4551c56a1da55
Margarita Anisimova <89300572+Margo363@users.noreply.github.com>
2022-07-05 22:54:08 +0300

    corrected "Создание таблиц"

--- lesson_02_tables_DDl_DML.sql
+++ lesson_02_tables_DDl_DML.sql
@@ -1,5 +1,4 @@
 -- создадим таблицу по доставке товара
-
 create table delivery(
 	delivery_id serial primary key,
 	address_id int references address(address_id) not null,
@@ -15,17 +14,14 @@ select * from delivery
 
 --добавим в таблицу с заказами ограничение внешнего ключа для столбца с 
 доставкой
-
 alter table orders add constraint orders_delivery_fkey foreign key (delivery_id)
 	references delivery(delivery_id)
 	
 --удаление внешнего ключа
-
 ALTER TABLE orders 
 DROP CONSTRAINT orders_delivery_newkey;
 
 --изменим данные в заказах, добавив данные по идентификатору доставки
-
 insert into delivery (address_id, delivery_date, time_range,staff_id)
 values (329, '2022.10.25', array['10:00:00''17:30:00'], 3),
 (9, '2022.09.25', array['10:00:00''17:30:00'], 1),
@@ -58,7 +54,6 @@ where order_id = 5
 select * from orders
 
 --попробуем удалить запись из доставки
-
 delete from delivery 
 where delivery_id = 1
 
