f3bd96f84b31256b0748f985a1356f895429c107
Margo363 <anisimova.post@gmail.com>
2022-07-06 01:37:43 +0300

    added "Cоздание отчетов и аналитика с помощью SQL"

--- lesson_04_SQL reporting and analytics.sql
+++ lesson_04_SQL reporting and analytics.sql
@@ -46,4 +46,32 @@ select order_id , customer_id , amount ,
 	avg(amount) over (partition by customer_id order by order_id)
 from orders	
 
+-- нужно постоянно получать данные по последнему заказу пользователя
+create view task_1 as
+select *
+from (
+select order_id , customer_id , amount ,
+	row_number () over (partition by customer_id order by order_id desc)
+from orders) t
+where row_number = 1
+
+select * from task_1 
+
+--нужно постоянно получать данные по последнему заказу пользователя с материализованным представлением
+create materialized view task_2 as
+select *
+from (
+select order_id , customer_id , amount ,
+	row_number () over (partition by customer_id order by order_id desc)
+from orders) t
+where row_number = 1
+with no data
+
+select * from task_2 
+
+--обновим данные в материализованном представлении
+refresh materialized view task_2 
+
+--удалим материализованное представление
+drop materialized view task_2 
 
